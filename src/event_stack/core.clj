37cd3693e82f42bd0a421b0846ebf31c187f478a
Logan Campbell-McPherson <colin@logaan.net>
2013-03-04 22:21:47 +1100

    output with colour

--- src/event_stack/core.clj
+++ src/event_stack/core.clj
@@ -1,8 +1,23 @@
 (ns event-stack.core 
-  (:import jline.console.ConsoleReader))
+  (:import jline.console.ConsoleReader
+           jline.UnixTerminal))
+
+(def codes {
+  :reset   "[0m"
+  :black   "[30m"
+  :red     "[31m"
+  :green   "[32m"
+  :yellow  "[33m"
+  :blue    "[34m"
+  :magenta "[35m"
+  :cyan    "[36m"
+  :white   "[37m"})
 
 (defn -main []
-  (println "Press a key:")
-  (let [charNum (.readCharacter (ConsoleReader.))]
-    (println (char charNum)))) 
+  (let [console (ConsoleReader.)]
+    (.clearScreen console)
+    (println "Press a key:")
+    (let [letter (-> console .readCharacter char str)]
+      (println (str (codes :reset) "You pressed: " (codes :magenta) letter))
+      (println (str (codes :reset) "You pressed: " (codes :red) letter)))))
 
