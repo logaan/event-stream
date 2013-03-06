3674cd2262b6860eecb955d4625272cb4a60202e
Logan Campbell-McPherson <colin@logaan.net>
2013-03-07 00:06:28 +1100

    added boundary checking to movement

--- src/event_stack/movement.clj
+++ src/event_stack/movement.clj
@@ -38,16 +38,29 @@
         move-events (vec (map move-event directions))]
     (update-in game [:events] (partial concat move-events))))
 
+(defn within-the-boundaries? [[x y]]
+  (and (<= 0 x 79) (<= 0 y 23)))
+
 (defn move-player [{{position :position} :player :as game} event]
   (let [offsets      (moves->offsets (:direction event))
         new-position (vec (map + position offsets))]
-    (assoc-in game [:player :position] new-position)))
+    (if (within-the-boundaries? new-position)
+      (assoc-in game [:player :position] new-position)
+      game)))
 
 (defn interpret-movement [game]
   (handle-events game :keypress convert-keypress-event)) 
 
 (defn move [game]
   (handle-events game :move move-player))
+
+; So I wanted to have a function that would consume any events that would cause
+; you to walk outside of the terminal. But the move step can consume multiple
+; walk events. Potentially none of which individually would step you outside,
+; but combined they would. So this interceptor function would have to consume
+; them all and know how they'd affect everyting. At that point it's
+; implementation is so tied to the other one that I'm not sure there's any
+; point.
 
 ; (move {:player {:position [10 10]}
 ;        :events [{:type :move :direction :down}]})
