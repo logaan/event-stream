4de620415c28f9a03bd6cc7ca525cf6f97071586
Logan Campbell-McPherson <colin@logaan.net>
2013-03-06 23:28:23 +1100

    refactored movement ns

--- src/event_stack/movement.clj
+++ src/event_stack/movement.clj
@@ -14,37 +14,48 @@
    \b [:down :left]
    \n [:down :right]})
 
-(defn move-event [direction]
-  {:type :move :direction direction})
-
-(defn interpret-movement [{[event & events] :events :as game}]
-  (if (= (:type event) :keypress)
-    (let [directions  (keys->moves (event :key))
-          move-events (vec (map move-event directions))]
-      (if (not (empty? directions))
-        (assoc-in game [:events] (concat move-events events))
-        game))
-    game))
-
 (def moves->offsets
   {:left  [-1 0]
    :right [+1 0]
    :up    [0 -1]
    :down  [0 +1]})
 
-(defn move [{[event & events] :events player :player :as game}]
-  (if (= (:type event) :move)
-    (let [offsets      (moves->offsets (:direction event))
-          new-position (vec (map + (:position player) offsets))]
-      (-> game
-          (assoc-in [:events] events)
-          (assoc-in [:player :position] new-position)))
-    game))
+(defn move-event [direction]
+  {:type :move :direction direction})
 
-; (move {:player {:position [10 10]}
-;        :events [{:type :move :direction :down}]})
-; 
-; (move {:player {:position [10 10]}
-;        :events []})
-;
+(defn is-event? [type]
+  (fn [event]
+    (= (:type event) type)))
+
+(defn handle-events [{events :events :as game} event-type handling-function]
+  (let [{relevant-events true other-events false} (group-by (is-event? event-type) events)]
+    (-> (reduce handling-function game relevant-events)
+        (assoc :events other-events))))
+
+(defn add-events [{events :events :as game} events]
+  (assoc-in game [:events] (concat move-events events)))
+
+(defn convert-keypress-event [game keypress-event]
+  (let [directions  (keys->moves (keypress-event :key))
+        move-events (vec (map move-event directions))]
+    (if (not (empty? directions))
+      (add-events game move-events) 
+      game)))
+
+(defn move-player [{{position :position} :player :as game} {direction :direction}]
+  (let [offsets      (moves->offsets direction)
+        new-position (vec (map + position offsets))]
+    (assoc-in game [:player :position] new-position)))
+
+(defn interpret-movement [game]
+  (handle-events game :keypress convert-keypress-event)) 
+
+(defn move [game]
+  (handle-events game :move move-player))
+
+(move {:player {:position [10 10]}
+       :events [{:type :move :direction :down}]})
+
+(move {:player {:position [10 10]}
+       :events []})
 
