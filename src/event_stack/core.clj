275048e2d0d41a8ced456f700643dba4ebdb2d9b
Logan Campbell <colin@logaan.net>
2013-03-13 23:01:28 +1100

    added documentation for core

--- src/event_stack/core.clj
+++ src/event_stack/core.clj
@@ -1,3 +1,5 @@
+;; The core ns exists to compose together all the pieces of the game at a high
+;; level and set them in motion.
 (ns event-stack.core 
   (:require
     [event-stack
@@ -5,23 +7,35 @@
      [terminal :as terminal]
      [movement :as movement]]))
 
+;; This is the bare minimum game. Any specifics should be added by setup
+;; functions.
 (def blank-game 
   {:events []})
 
-(defn setup [game]
+(defn setup!
+  "Prepares a blank game for start of play."
+  [game]
   (-> game
       movement/setup  
       terminal/setup))
 
-(defn teardown [game]
+(defn cleanup!
+  "Closes and finalises anything opened during startup."
+  [game]
   (-> game
       terminal/teardown))
 
-(defn event-sources! [game]
+(defn event-sources!
+  "Provides events from the outside world such as user input."
+  [game]
   (-> game
       terminal/get-keypress!))
 
-(defn event-handlers [game]
+(defn event-handlers
+  "Purely functional core. It should take any events and process them. These
+  handlers may emit events of their own and will continue to be run until all
+  events have been consumed."
+  [game]
   (-> game
       terminal/handle-exit
       movement/interpret-movement
@@ -29,23 +43,35 @@
       movement/prevent-movement-outside-boundaries
       movement/update-position))
 
-(defn event-sinks! [game]
+(defn update-views!
+  "Views are any side-effecty code that wants to know about the state of the
+  game. They will only be updated once all internal event firing has died dow,
+  and so they should present a consistent view of the world."
+  [game]
   (-> game
       terminal/draw-screen!))
 
-(defn process-events [game]
+(defn process-events
+  "Runs event-handlers until they have consumed all new events and stopped
+  emitting events of their own."
+  [game]
   (if (empty? (:events game)) 
     game (recur (event-handlers game))))
 
-(defn process-input [game]
-  (recur (-> game event-sources! process-events event-sinks!)))
+(defn process-input
+  "The outer game loop. It takes events from the outside world, runs them
+  through our functional core and then notifies views of the latest state."
+  [game]
+  (recur (-> game event-sources! process-events update-views!)))
 
-(defn -main []
-  (let [setup-game (setup blank-game)]
+(defn -main
+  "Handles setup, starts the game loop and ensures any cleanup occurs."
+  []
+  (let [setup-game (setup! blank-game)]
     (try
       (process-input setup-game)
       (catch Exception e
         (throw e))
       (finally
-        (teardown setup-game)))))
+        (cleanup! setup-game)))))
 
